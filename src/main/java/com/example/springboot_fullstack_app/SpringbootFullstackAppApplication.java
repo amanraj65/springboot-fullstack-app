a6e7ef36c79b03f2fa92ab20451428fd0320b7ce
amanraj65 <amanraj.mca23@bmsce.ac.in>
2025-07-09 15:51:40 +0530

    Initial project setup

---
+++ src/main/java/com/example/springboot_fullstack_app/SpringbootFullstackAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_fullstack_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootFullstackAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootFullstackAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springboot_fullstack_app/SpringbootFullstackAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_fullstack_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootFullstackAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
